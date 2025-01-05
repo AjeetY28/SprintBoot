f39d39c76b4aeeb1daaa4ab1e639fc5a5dcca507
AjeetY28 <ajeet.bca28@gmail.com>
2025-01-06 01:26:43 +0530

    1.2 creating beans

---
+++ src/main/java/com/strangeSpringBootIntroduction/IntroductionToSpringBoot/Apple.java
@@ -0,0 +1,11 @@
+package com.strangeSpringBootIntroduction.IntroductionToSpringBoot;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Apple {
+    void eatApple()
+    {
+        System.out.println("I am eating the apple");
+    }
+}
